80c67261f99364f69d55c0d5e116946bc2075174
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-09-20 18:53:39 +0530

    Added the vercel files

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,19 @@
 require("dotenv").config()
 const Sequelize = require("sequelize")
 
-const sequelize=new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    dialectModule: require('mysql2')
-   
-  });
-
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  dialectModule: require('mysql2'),
+  dialectOptions: {
+      ssl: {
+          require: true, 
+          rejectUnauthorized: false 
+      }
+  },
+  logging: false,
+  
+});
 
 module.exports=sequelize
